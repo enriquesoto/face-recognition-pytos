eaa6480d9cffd03db8bd33095ffd823355e3a9d7
enrique <e.sotomendoza@gmail.com>
2015-10-30 01:23:33 -0500

    changing daemon

--- pytos/pytos_daemon.py
+++ pytos/pytos_daemon.py
@@ -1,44 +1,24 @@
-import multiprocessing
+# main definitions
 import time
-import pdb
+import rpyc
+from rpyc.utils.server import ThreadedServer
+from threading import Thread
 
+class Global():
+    offloading=False
 
-class PytosDaemon(multiprocessing.Process):
+class MyService(rpyc.Service):
+    def exposed_getOffloadingDesicion(self):
+        return main.offloading
 
-	def __init__(self, task_queue, result_queue):
-		multiprocessing.Process.__init__(self)
-		self.daemon=True
-		self.task_queue = task_queue
-		self.result_queue = result_queue
-	
-	def run(self):
-		proc_name = self.name
-		while True:
-			next_task = self.task_queue.get()
-			if next_task is None:
-				#a poisson flag means shutdown
-				print ':s: Exiting' % proc_name
-				self.task_queue.task_done()
-				break
-			print '%s: %s' %(proc_name,next_task)
-			answer = next_task()
-			self.task_queue.task_done()
-			self.result_queue.put(answer)
-		return
+if __name__ == '__main__':
+	server = ThreadedServer(MyService, port = 12345)
+	t = Thread(target = server.start)
+	t.daemon = True
+	t.start()
 
-class Solver(object):
-	def __init__(self, a, b):
-		self.a=a
-		self.b=b
-	def __call__(self):
-		return False
-
-if __name__=='__main__'
-	solver = multiprocessing.JoinableQueue()
-	result = multiprocessing.Queue()
-	d = multiprocessing.Process(name='daemon', target=daemon)
-	d.daemon = True
-	d.start()
-
-
-
+# the main logic
+	main = Global()
+	while True:
+		main.offloading = True
+		time.sleep(3)
